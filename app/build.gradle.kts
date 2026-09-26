793a212277e27cdfdb9e9f1f0c271b63b431782b
zenyxx-xd <zenyxx-xd@users.noreply.github.com>
2026-09-26 14:16:58 +0300

    1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         targetSdk = 36
         // Схема MMmmpp: 01-мажор 00-минор 10-патчи
         versionCode = 10100
-        versionName = "1.1.0"
+        versionName = "1.1.1"
         buildConfigField(
             "String", "RMV_DISPATCH_TOKEN",
             "\"${reportProps.getProperty("dispatchToken", "")}\"",
